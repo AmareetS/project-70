2ca4297a5523dd3d4772bb31b31fc3eeea2bed87
AmareetS <68671075+AmareetS@users.noreply.github.com>
2021-05-25 17:54:36 -0700

    Update WriteStoryScreen.js

--- WriteStoryScreen.js
+++ WriteStoryScreen.js
@@ -16,9 +16,31 @@ export default class WriteStoryScreen extends React.Component {
 
     render() {
         return(
-        <AppContainer></AppContainer>
+        <View style={styles.container}> 
+            <Header backgroundColor = {'pink'} centerComponent = 
+                {{ text : 'Story Hub', style : { color: 'black', fontSize: 30} }} />
+        <TextInput placeholder="Story Title" onChangeText= 
+            {(text)=>{ this.setState({ title: text }) }} 
+            value={this.state.title} 
+            style={styles.title} placeholderTextColor='black'/>
+        <TextInput placeholder="Author" onChangeText= 
+            {(text)=>{ this.setState({ author: text }) }} 
+            placeholderTextColor='black' 
+            value={this.state.author} 
+            style={styles.author} />
+        <TextInput placeholder="Write your story" onChangeText= 
+            {(text)=>{ this.setState({ storyText: text }) }} 
+            placeholderTextColor='black' value={this.state.storyText} 
+            style={styles.storyText} 
+            multiline={true}/>
+         <TouchableOpacity style={styles.submitButton} 
+            // onPress={this.submitStory} > 
+            <Text style={styles.buttonText}>
+             Submit</Text> 
+                   </TouchableOpacity> 
+                    </View>
         );
-      }
+      }}
 
   const styles = StyleSheet.create({
     container: {
@@ -26,35 +48,38 @@ export default class WriteStoryScreen extends React.Component {
       justifyContent: 'center',
       alignItems: 'center'
     },
-    displayText:{
-      fontSize: 15,
-      textDecorationLine: 'underline'
+    title: { 
+        height: 40, 
+        borderWidth: 2, 
+        marginTop: 40, 
+        margin: 10, 
+        color:'black', 
+        padding: 6, 
     },
-    scanButton:{
-      backgroundColor: '#2196F3',
-      padding: 10,
-      margin: 10
+    author: { 
+        height: 40, 
+        borderWidth: 2, 
+        margin: 10, 
+        padding: 6, 
     },
-    buttonText:{
-      fontSize: 15,
-      textAlign: 'center',
-      marginTop: 10
+    storyText: { 
+        height: 250, 
+        borderWidth: 2, 
+        margin: 10, 
+        padding: 6, 
     },
-    inputView:{
-      flexDirection: 'row',
-      margin: 20
-    },
-    inputBox:{
-      width: 200,
-      height: 40,
-      borderWidth: 1.5,
-      borderRightWidth: 0,
-      fontSize: 20
-    },
-    scanButton:{
-      backgroundColor: '#66BB6A',
-      width: 50,
-      borderWidth: 1.5,
-      borderLeftWidth: 0
+    submitButton:{ 
+        justifyContent: 'center', 
+        alignSelf: 'center', 
+        backgroundColor: 'blue', 
+        width: 80, 
+        height: 40,
+        color:'black', 
+    }, 
+    buttonText: { 
+        textAlign: 'center', 
+        color: 'white', 
+        fontWeight: 'bold', 
+        color:'black', 
     }
   });
